0ecbab09a6e36b8837620bd60348ec04209994ac
Adetoun <adetotounadebowale@gmail.com>
2026-02-22 16:50:21 +0100

    images to public

--- src/data.jsx
+++ src/data.jsx
@@ -1,15 +1,17 @@
 
 
 const products = [
-  { id: 1, name: "Waffle with Berries", price: 6.5, image: "../src/assets/images/image-waffle-desktop.jpg" },
-  { id: 2, name: "Vanilla Bean Crème Brûlée", price: 7.0, image: "../src/assets/images/image-creme-brulee-desktop.jpg" },
-  { id: 3, name: "Macaron Mix of Five", price: 8.0, image: "../src/assets/images/image-macaron-desktop.jpg" },
-  { id: 4, name: "Classic Tiramisu", price: 5.5, image: "../src/assets/images/image-tiramisu-desktop.jpg" },
-  { id: 5, name: "Pistachio Baklava", price: 4.0, image: "../src/assets/images/image-baklava-desktop.jpg" },
-  { id: 6, name: "Lemon Meringue Pie", price: 5.0, image: "../src/assets/images/image-meringue-desktop.jpg" },
-  { id: 7, name: "Red Velvet Cake", price: 4.5, image: "../src/assets/images/image-cake-desktop.jpg" },
-  { id: 8, name: "Salted Caramel Brownie", price: 5.5, image: "../src/assets/images/image-brownie-desktop.jpg" },
-  { id: 9, name: "Vanilla Panna Cotta", price: 6.5, image: "../src/assets/images/image-panna-cotta-desktop.jpg" }
+  { id: 1, name: "Waffle with Berries", price: 6.5, image: "image-waffle-desktop.jpg" },
+  { id: 2, name: "Vanilla Bean Crème Brûlée", price: 7.0, image: "image-creme-brulee-desktop.jpg" },
+  { id: 3, name: "Macaron Mix of Five", price: 8.0, image: "image-macaron-desktop.jpg" },
+  { id: 4, name: "Classic Tiramisu", price: 5.5, image: "image-tiramisu-desktop.jpg" },
+  { id: 5, name: "Pistachio Baklava", price: 4.0, image: "image-baklava-desktop.jpg" },
+  { id: 6, name: "Lemon Meringue Pie", price: 5.0, image: "image-meringue-desktop.jpg" },
+  { id: 7, name: "Red Velvet Cake", price: 4.5, image: "image-cake-desktop.jpg" },
+  { id: 8, name: "Salted Caramel Brownie", price: 5.5, image: "image-brownie-desktop.jpg" },
+  { id: 9, name: "Vanilla Panna Cotta", price: 6.5, image: "image-panna-cotta-desktop.jpg" }
 ];
 
 export default products;
+
+/* "../src/assets/images/image-panna-cotta-desktop.jpg"*/
